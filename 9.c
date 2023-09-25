aa5d674f69002f5a604c1cd99646ed186fb9245c
Dheeraj <dheerajkumar6112003@gmail.com>
2023-09-25 18:46:08 +0530

    KBC Software in switch case

---
+++ 9.c
@@ -0,0 +1,438 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("game rules:\n");
+    printf(" In this game 14 questions are available Each question carry some cash amount\n");
+    printf("Q1. 5,000 Rs\n Q2. 10,000\n, Q3. 20,000 Rs\n,Q4. 40,000 Rs\n, Q5. 80,000 Rs\n");
+    printf("Q6. 1,60,000 Rs\n Q7. 3,20,000\n, Q8. 6,40,0000 Rs\n,Q9. 12,50,000 Rs\n,10. 25,00,000 Rs\n");
+    printf("Q11. 50,00,000 Rs\n Q12. 1 Crore\n, Q13. 3 Crore Rs\n,Q14. 7 crore Rs\n");
+
+    printf("Lets start our game this is quiz game\n\n");
+    printf("our 1st question is of 5000 Rs\n");
+    printf(" Q1. The India Prime Minister Narendra Modi recently inaugrated check Post at Jogbani-Biratnagar border, between India and which country ?\n");
+    printf("1. Bangladesh\n");
+    printf("2. Nepal\n");
+    printf("3. Bhutan\n");
+    printf("4. Myanmar\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 2. that is Nepal you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("congratulation your answer is correct you win 5,000 Rs \n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 2. that is Nepal you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 2. that is Nepal you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+    printf("Next Question is 10,000 Rs\n");
+    printf("Our 2nd Question is \n");
+    printf("Q2. India has recently signed five MoUs with which country, to develop its educational infrastructure?\n");
+    printf("1. Ghana\n");
+    printf("2. Afghanistan\n");
+    printf("3. Malawi\n");
+    printf("4. Syria\n");
+    printf("Enter your number = ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 2. that is Afganistan, Now you loss ypur amount\n\n\n");
+        break;
+
+    case 2:
+        printf("congratulation your answer is correct, Now you win 10,000 Rs\n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 2. that is Afghanistan, Now you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 2. that is Afghanistan, Now you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("Next Question is 20,000 Rs \n");
+    printf("Our 3rd Question is \n");
+    printf("Q3. The National Medicinal Plant Board(NMPB) works under which Union Ministry?\n");
+    printf("1. Ministry of Agriculture and Farmers Welfare\n");
+    printf("2. Ministry of Health and Family Welfare\n");
+    printf("3. Ministry of AYUSH\n");
+    printf("4. Ministry of MSME\n");
+    printf("Enter your number = ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is Ministry of AYUSH, Now you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf(" Your answer is wrong correct answer is 3. that is Ministry of AYUSH, Now you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct,you win 20,000 Cash price\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. Ministry of AYUSH, Nowyou loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 4rt question is of 40,000 Rs\n");
+    printf(" Q4. Which Union Ministry has notified the standards for Safety Evaluation of Hydrogen Fuels ?\n");
+    printf("1. Ministry of Power\n");
+    printf("2. Ministry of Renewable Energy\n");
+    printf("3. Ministry of Road Transport and Highways\n");
+    printf("4. Ministry of MSME\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is Ministry of Road and Transport and Highways you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 3. that is Ministry of Road and Transport and Highways you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct you win 40,000 Rs \n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. that is Ministry of Road and Transport and Highways you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("Next Question is 80,000 Rs \n");
+    printf("Our 5th Question is \n");
+    printf("Q5. Which company has won the bid for the largest segment of Mumbai-Ahmedabad high speed rail project?\n");
+    printf("1. Afcons Infrastructure\n");
+    printf("2. GMR Infrastructure\n");
+    printf("3. Adani Group\n");
+    printf("4. Larse and Toubro\n");
+    printf("Enter your number = ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 4. that is Larse and Toubro, Now you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf(" Your answer is wrong correct answer is 4. that is Larse and Toubro, Now you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 4. that is Larse and Toubro, Now you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("congratulation your answer is correct,you win 80,000 Cash price\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 6st question is of 1,60,000 Rs\n");
+    printf(" Q6. Which Public Sector Company has been granted permission to use drones for inspection activities?\n");
+    printf("1. ONGC\n");
+    printf("2. Indian Oil Corporation Limited\n");
+    printf("3. Oil India Limited\n");
+    printf("4. NTPC Limited\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 4. that is NTPC limited you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 4. that is NTPC limited you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 4. that is NTPC limited you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("congratulation your answer is correct you win 1,60,000 Rs\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 7nt question is of 3,20,000 Rs\n");
+    printf(" Q7. India has decided to explore connectivity routes via INST Corridor, along with which country, during its virtual bilateral summit?\n");
+    printf("1. Uzbekistan\n");
+    printf("2. Russia\n");
+    printf("3. Iran\n");
+    printf("4. Azerbaijan\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("congratulation your answer is correct you win 3,20,000 Rs\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 1. that is Uzebekistan you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 1. that is Uzebekistan you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 1. that is Uzebekistan you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 8rt question is of 6,40,000 Rs\n");
+    printf(" Q8. Which state assembly recently passed a resolution to abolish the state's legislative council?\n");
+    printf("1. Telangana\n");
+    printf("2. Maharashtra\n");
+    printf("3. Andhra Pradesh\n");
+    printf("4. Karnataka\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is Andhra Pradesh you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 3. that is Andhra Pradesh you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct you win 6,40,000 Rs\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. that is Andhra Pradesh you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 9nt question is of 12,50,000 Rs\n");
+    printf(" Q9. Arvind Kejriwal led-Aam Aadmi Party won how many seats in the Delhi assembly elections?\n");
+    printf("1. 42\n");
+    printf("2. 52\n");
+    printf("3. 62\n");
+    printf("4. 72\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is 62 you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 3. that is 62 you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct you win 12,50,0000 Rs\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. that is 62 you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("Next Question is 25,00,000 Rs\n");
+    printf("Our 10th Question is \n");
+    printf("Q10.What is the term of the 22nd Law Commission of India, recently approved by the Union Cabinet of India?\n");
+    printf("1. 2 years\n");
+    printf("2. 3 years\n");
+    printf("3. 5 years\n");
+    printf("4. 6 years\n");
+    printf("Enter your number = ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 2. that is 3 years, Now you loss ypur amount\n\n\n");
+        break;
+
+    case 2:
+        printf("congratulation your answer is correct, Now you win 25,00,000 Rs\n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 2. that is 3 years, Now you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 2. that is 3 years, Now you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 11th question is of 50,00,0000 Rs\n");
+    printf(" Q11. As per the recent notification from the union Ministry of Home Affairs(MHA), which industry has been exempted from the lockdown?\n");
+    printf("1. Coal Mining\n");
+    printf("2. Fishing and Aquaculture\n");
+    printf("3. Textile\n");
+    printf("4. Automobile\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 2. that is Fishing and Aquaculture you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("congratulation your answer is correct you win 50,00,000 Rs \n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 2. that is Fishing and Aquaculture you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 2. that is Fishing and Aquaculture you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 12th question is of 1 crore Rs\n");
+    printf(" Q12. Which is the first Indian state to get approval from the Indian Council for medical Reserch(ICMR) for pool testing the samples for Covid-19?\n");
+    printf("1. Kerala\n");
+    printf("2. Maharashtra\n");
+    printf("3. Uttar Pradesh\n");
+    printf("4. Rajasthan\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is Uttar Pradesh you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 3. that is Uttar Pradesh you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct you win 1 crore Rs\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. that is Uttar Pradesh you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 13nt question is of 3 crores \n");
+    printf(" Q13. Which major technology company launched the second phase of 'More Together' campaign in India?\n");
+    printf("1. Amazon\n");
+    printf("2. Microsoft\n");
+    printf("3. Facebook\n");
+    printf("4. Google\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 3. that is Facebook you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("Your answer is wrong correct answer is 3. that is Facebook you loss your amount\n\n\n");
+        break;
+
+    case 3:
+        printf("congratulation your answer is correct you win 3 crore Rs\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 3. that is Facebook you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf("our 14th question is of 7 crore Rs\n");
+    printf(" Q14. The Information & Broadcasting Ministry has launched a dedicated Door Darshan channel for which state?\n");
+    printf("1. Meghalaya\n");
+    printf("2. Assam\n");
+    printf("3. Manipur\n");
+    printf("4. Sikkim\n");
+
+    printf("Enter your answer = ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Your answer is wrong correct answer is 2. that is Assam you loss your amount\n\n\n");
+        break;
+
+    case 2:
+        printf("congratulation your answer is correct you win 7 crores Rs \n\n\n");
+        break;
+
+    case 3:
+        printf("Your answer is wrong correct answer is 2. that is Assam you loss your amount\n\n\n");
+        break;
+
+    case 4:
+        printf("Your answer is wrong correct answer is 2. that is Assam you loss your amount\n\n\n");
+        break;
+    default:
+        printf(" Your number is invailid\n\n\n");
+    }
+
+    printf(" congratulations you finished your game\n");
+    printf(" now our game is finished");
+    return 0;
+}
